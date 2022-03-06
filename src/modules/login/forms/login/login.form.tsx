db9b94ea08e9a7200c90369eb5b72c34abe8b302
Kennedy Silva <kennedy2000silva@hotmail.com>
2022-03-06 20:56:13 -0300

    fix: unused imports

--- src/modules/login/forms/login/login.form.tsx
+++ src/modules/login/forms/login/login.form.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {TextField} from '../../../../shared/components/TextField';
-import {Text, TouchableOpacity} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import {UIButton} from '../../../../shared';
 import {useFormik} from 'formik';
 import * as yup from 'yup';
 import {FormProps} from '../../../../shared/components/form';
+
+import {Icon} from 'react-native-eva-icons';
+import {useTheme} from '@react-navigation/native';
 
 const LoginFormSchema = yup.object().shape({
   email: yup.string().email().required(),
@@ -18,22 +21,25 @@ interface LoginData {
 
 type LoginFormProps = FormProps<LoginData>;
 
-export function LoginForm() {
-  const {values, handleChange, handleSubmit, errors} = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: LoginFormSchema,
-    onSubmit: fields => {
-      console.log(fields);
-    },
-  });
+export function LoginForm(props: LoginFormProps) {
+  const {values, handleChange, handleSubmit, errors, touched, handleBlur} =
+    useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      validationSchema: LoginFormSchema,
+      onSubmit: fields => {
+        props.onComplete(fields);
+      },
+    });
 
   const [seePassword, setSeePassword] = React.useState(true);
   const toggleSeePassword = () => {
     setSeePassword(!seePassword);
   };
+
+  const {colors} = useTheme();
 
   return (
     <>
@@ -44,23 +50,28 @@ export function LoginForm() {
         placeholder={'example@email.com'}
         keyboardType={'email-address'}
         textContentType={'emailAddress'}
-        error={errors.email}
+        onBlur={handleBlur('email')}
+        error={touched.email ? errors.email : undefined}
       />
       <TextField
         label={'Senha'}
         value={values.password}
+        onBlur={handleBlur('password')}
         onChangeText={handleChange('password')}
         placeholder={'password'}
         secureTextEntry={seePassword}
         textContentType={'password'}
-        error={errors.password}
         endIcon={
           <TouchableOpacity onPress={toggleSeePassword}>
-            <Text>Olho </Text>
+            <Icon
+              name={seePassword ? 'eye-off' : 'eye'}
+              width={24}
+              height={24}
+              fill={colors.text}
+            />
           </TouchableOpacity>
         }
       />
-
       <UIButton onPress={handleSubmit} title={'Fazer login'} />
     </>
   );
